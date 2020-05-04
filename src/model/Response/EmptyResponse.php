c672823329ab9265254432fc4460cfd650d522c7
Peter Dulacka <dulacka@gmail.com>
2020-05-04 20:18:58 +0000

    Adding handler for sending Mobiletech (sms) messages
    - Added `MobiletechNotificationEvent` for notification
    specific to the module - such as response to the
    received message.
    - Added universal `NotificationHandler` and `SendHandler`
    to process outbound messages. All incoming and outgoing
    messages are now being stored.
    - Added `TestInboundHandler` and `TestNotificationCommand`
    to manually test processing of received messages.
    See module's README file for more details.
    - Added configuration option for definition of free bill key.
    This is aimed to be used for generic purpose where we know
    that the message is not monetized.
    - Fixed inconsistency between modified_at/updated_at columns.
    
    Co-Authored-By: Dominik Skerhak <dominik.skerhak@gmail.com>
    
    remp/crm#1171

---
+++ src/model/Response/EmptyResponse.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Crm\ApiModule\Api;
+
+use Crm\ApiModule\Response\ApiResponseInterface;
+use Nette;
+use Nette\Http\Response;
+
+class EmptyResponse implements ApiResponseInterface
+{
+    private $httpCode = Response::S204_NO_CONTENT;
+
+    public function setHttpCode($httpCode)
+    {
+        $this->httpCode = $httpCode;
+    }
+
+    public function getHttpCode()
+    {
+        return $this->httpCode;
+    }
+
+    public function send(Nette\Http\IRequest $httpRequest, Nette\Http\IResponse $httpResponse)
+    {
+        // nothing, void, emptiness...
+    }
+}

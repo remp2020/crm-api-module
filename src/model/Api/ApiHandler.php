0d94c0bb600c93d7a4a7d0fd580760728c446527
Miroslav Svítok <mirosvitok@gmail.com>
2020-05-06 14:48:11 +0200

    Processing of password reset text message in Mobiletech API
    This change includes:
    - mockable raw input for ApiHandler
    - mobiletech outbound message user is nullable (for anonymous users)
    - nullable user attribute for NotificationEvent (for anonymous users)
    - refactoring in UserManager
    
    remp/crm#1198

--- src/model/Api/ApiHandler.php
+++ src/model/Api/ApiHandler.php
@@ -2,13 +2,46 @@
 
 namespace Crm\ApiModule\Api;
 
+use Crm\ApiModule\Authorization\NoAuthorization;
+
 abstract class ApiHandler implements ApiHandlerInterface
 {
     private $idempotentKey = null;
 
+    private $mockedRawPayloadsQueue = [];
+
     public function resource(): string
     {
         return self::resourceFromClass(static::class);
+    }
+
+    /**
+     * Push mocked 'php://input' payload into payload queue. Useful for testing
+     * @param $content
+     */
+    public function mockRawPayload($content)
+    {
+        $this->mockedRawPayloadsQueue[] = $content;
+    }
+
+    /**
+     * Retrieve raw payload from 'php://input' (or mocked content in case of tests)
+     * @return false|mixed|string
+     */
+    public function rawPayload()
+    {
+        if ($this->mockedRawPayloadsQueue) {
+            return array_shift($this->mockedRawPayloadsQueue);
+        }
+        return file_get_contents('php://input');
+    }
+
+    /**
+     * Handle to call in tests
+     */
+    public function handleInTest()
+    {
+        return $this->handle(new NoAuthorization());
     }
 
     public static function resourceFromClass($className): string

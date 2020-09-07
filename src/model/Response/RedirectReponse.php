8de9639ac74d27f66c02381bdaeb9c6d7c3382cb
Tomas Majer <tomasmajer@gmail.com>
2020-09-07 23:14:50 +0200

    Mail clicks tracking

---
+++ src/model/Response/RedirectReponse.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Crm\ApiModule\Api;
+
+use Crm\ApiModule\Response\ApiResponseInterface;
+use Nette;
+use Nette\Http\Response;
+
+class RedirectResponse implements ApiResponseInterface
+{
+    private $httpCode = Response::S307_TEMPORARY_REDIRECT;
+
+    private $url;
+
+    public function __construct(string $url)
+    {
+        $this->url = $url;
+    }
+
+    public function setHttpCode($httpCode)
+    {
+        $this->httpCode = $httpCode;
+    }
+
+    public function getHttpCode()
+    {
+        return $this->httpCode;
+    }
+
+    public function send(Nette\Http\IRequest $httpRequest, Nette\Http\IResponse $httpResponse)
+    {
+        header("Location: {$this->url}");
+    }
+}

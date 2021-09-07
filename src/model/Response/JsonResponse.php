50dffcc0b7dfa567fda7ac030d70bbe3bfea28e9
Dominik Skerhak <dominik.skerhak@gmail.com>
2021-09-07 22:21:55 +0200

    Nette v3.0 - Removed Crm\ApiModule\Api\JsonResponse parent
    It doesn't extend `Nette\Application\Responses\JsonResponse` anymore.
    Nette's `JsonResponse` is now final class.
    
    This isn't breaking change, CRM doesn't expect use of parent class in APIs.
    
    remp/crm#1979

--- src/model/Response/JsonResponse.php
+++ src/model/Response/JsonResponse.php
@@ -3,15 +3,53 @@
 namespace Crm\ApiModule\Api;
 
 use Crm\ApiModule\Response\ApiResponseInterface;
-use Nette\Application\Responses\JsonResponse as NetteJsonResponse;
+use Nette\Http\IRequest;
+use Nette\Http\IResponse;
+use Nette\SmartObject;
+use Nette\Utils\Json;
 
-class JsonResponse extends NetteJsonResponse implements ApiResponseInterface
+class JsonResponse implements ApiResponseInterface
 {
+    use SmartObject;
+
+    /** @var mixed */
+    private $payload;
+
+    /** @var string */
+    private $contentType;
+
     private $httpCode;
 
-    public function __construct($payload)
+    public function __construct($payload, string $contentType = null)
     {
-        parent::__construct($payload, 'application/json; charset=utf-8');
+        $this->payload = $payload;
+        $this->contentType = $contentType ?: 'application/json; charset=utf-8';
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getPayload()
+    {
+        return $this->payload;
+    }
+
+    /**
+     * Returns the MIME content type of a downloaded file.
+     */
+    public function getContentType(): string
+    {
+        return $this->contentType;
+    }
+
+    /**
+     * Sends response to output.
+     */
+    public function send(IRequest $httpRequest, IResponse $httpResponse): void
+    {
+        $httpResponse->setContentType($this->contentType, 'utf-8');
+        echo Json::encode($this->payload);
     }
 
     public function getHttpCode()

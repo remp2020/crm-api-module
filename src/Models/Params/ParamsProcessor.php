01ac6685456282eccc95ed439e00fbb13c7c5f36
Dominik Skerhak <dominik.skerhak@gmail.com>
2022-01-28 23:42:48 +0100

    Nette API: ParamsProcessor now extends NetteApi's ParamProcessor
    `isError()` now returns bool only. In case you still need `isError()`
    to return first encountered error, use temp method `hasError()` (which
    is copy of previous method). But you'll need to fix this in future and
    use `isError()` together with `getErrors()`.
    
    This:
    
    ```php
    $error = $paramsProcessor->hasError();
    if ($error) {
        $response = new JsonResponse([
            'status' => 'error',
            'message' => 'Wrong input - ' . $error,
        ]);
    }
    ```
    
    Should be fixed to:
    
    ```php
    if ($paramsProcessor->isError()) {
        $response = new JsonResponse([
            'status' => 'error',
            'code' => 'invalid_request',
            'errors' => $paramsProcessor->getErrors(),
        ]);
    }
    ```
    
    Unfortunately this is breaking change so we are not pushing this forward
    right now.
    
    remp/crm#333

--- src/Models/Params/ParamsProcessor.php
+++ src/Models/Params/ParamsProcessor.php
@@ -2,32 +2,32 @@
 
 namespace Crm\ApiModule\Params;
 
-class ParamsProcessor
+class ParamsProcessor extends \Tomaj\NetteApi\Params\ParamsProcessor
 {
     /** @var ParamInterface[] */
     private $params;
 
-    public function __construct(array $params)
+    /**
+     * @see hasError() if you need first returned error.
+     */
+    public function isError(): bool
     {
-        $this->params = $params;
+        return parent::isError();
     }
 
-    public function isError()
+    /**
+     * This is temporary method. isError() now returns only boolean. In case you used returned error string from isError() and you don't want to rewrite your code now, use hasError().
+     *
+     * @deprecated use isError() to find out if there is error and getErrors() to get errors.
+     */
+    public function hasError()
     {
-        foreach ($this->params as $param) {
-            if (!$param->isValid()) {
-                return "Invalid value for {$param->getKey()}";
-            }
+        if (!$this->isError()) {
+            return false;
         }
-        return false;
-    }
 
-    public function getValues()
-    {
-        $result = [];
-        foreach ($this->params as $param) {
-            $result[$param->getKey()] = $param->getValue();
-        }
-        return $result;
+        $errors = $this->getErrors();
+        $paramKey = key(reset($errors));
+        return "Invalid value for {$paramKey}";
     }
 }

c8a3fe6fe4183942643b3af023c2ae1d0513b98c
Peter Dulacka <dulacka@gmail.com>
2023-02-15 08:37:26 +0100

    Use explicit method with JsonApiResponse response type for API tests
    remp/crm#2761

--- src/Tests/ApiTestTrait.php
+++ src/Tests/ApiTestTrait.php
@@ -4,6 +4,7 @@ namespace Crm\ApiModule\Tests;
 
 use Crm\ApiModule\Api\ApiHandlerInterface;
 use Crm\ApiModule\Api\Runner;
+use Tomaj\NetteApi\Response\JsonApiResponse;
 use Tomaj\NetteApi\Response\ResponseInterface;
 
 trait ApiTestTrait
@@ -16,5 +17,12 @@ trait ApiTestTrait
             $this->apiRunner = $this->inject(Runner::class);
         }
         return $this->apiRunner->run($apiHandler);
+    }
+
+    public function runJsonApi(ApiHandlerInterface $apiHandler): JsonApiResponse
+    {
+        $response = $this->runApi($apiHandler);
+        $this->assertInstanceOf(JsonApiResponse::class, $response);
+        return $response;
     }
 }

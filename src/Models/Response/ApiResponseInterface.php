f52c6c4108d5a6f50856e09821197aefd063fede
Dominik Skerhak <dominik.skerhak@gmail.com>
2025-03-31 12:37:02 +0200

    Remove resolved todos
    remp/crm#2368

--- src/Models/Response/ApiResponseInterface.php
+++ src/Models/Response/ApiResponseInterface.php
@@ -5,9 +5,6 @@ namespace Crm\ApiModule\Models\Response;
 
 use Tomaj\NetteApi\Response\ResponseInterface;
 
-// TODO[crm#2337]: We could probably remove also this interface. It is not used as return in open source.
-//                 ApiModules' EmptyResponse, ContentModule's ContentResponse & CoverpageModule's Response
-//                 can all extend directly ResponseInterface and have internal setCode() without interface.
 interface ApiResponseInterface extends ResponseInterface
 {
     public function getCode(): int;
